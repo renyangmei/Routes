bd7886a99aff1e3aa897f92ed362c8cc991997a5
better <shadowan@foxmail.com>
2017-06-12 11:40:15 +0800

    component

--- js/index.js
+++ js/index.js
@@ -1,57 +1,60 @@
 var app=angular.module('DemoApp',['ngRoute']);
+/*路由*/
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/',{
         templateUrl:'tpl/main.html',
         controller:"main"
-    }).when('/first',{
-        templateUrl:'tpl/first.html',
-        controller:"first"
-    }).when('/two',{
-        templateUrl:'tpl/two.html',
-        controller:"two"
-    }).when('/three',{
-        templateUrl:'tpl/three.html',
-        controller:"three"
+    }).when('/mail',{
+        templateUrl:'tpl/mail.html',
+        controller:"mail"
+    }).when('/phone',{
+        templateUrl:'tpl/phone.html',
+        controller:"phone"
+    }).when('/setting',{
+        templateUrl:'tpl/setting.html',
+        controller:"setting"
     })
 }]);
-
-
+/*控制器*/
 app.controller("main",["$scope","$http",function($scope,$http){
     $scope.title="首页";
     $http({
         url:'data/data.json',
         method:"get"
     }).then(function (res) {
+        // alert(1);
         $scope.names = res.data;
         console.log(res);
         console.log($scope.names);
         console.log(res.data)
     })
-}])
-app.controller("first",["$scope","$http",function($scope,$http){
+}]);
+app.controller("mail",["$scope","$http",function($scope,$http){
+    $scope.title="邮件";
+    $http({
+        url:'data/data.json',
+        method:"get"
+    }).then(function (res) {
+        // alert(2);
+        $scope.names = res.data;
+    })
+}]);
+app.controller("phone",["$scope","$http",function($scope,$http){
     $scope.title="电话";
     $http({
         url:'data/phone.json',
         method:"get"
     }).then(function (res) {
         $scope.phone = res.data;
-        console.log(res);
-        console.log($scope.phone);
-        console.log(res.data)
     })
-}])
-app.controller("two",["$scope","$http",function($scope,$http){
-    $scope.title="邮件";
-    $http({
-        url:'data/data.json',
-        method:"get"
-    }).then(function (res) {
-        $scope.names = res.data;
-        console.log(res);
-        console.log($scope.names);
-        console.log(res.data)
-    })
-}])
-app.controller("three",["$scope","$http",function($scope,$http){
+}]);
+app.controller("setting",["$scope","$http",function($scope,$http){
     $scope.title="设置";
-}])
+}]);
+/*组件*/
+app.component("pageHeader",{
+    templateUrl:"./tpl/component/head/pageheader.html",
+    controller:function ($scope) {
+        $scope.title="页面"
+    }
+});
